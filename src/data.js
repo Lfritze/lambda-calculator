1c622801382717c62e0fc848a7c34fc9097c5bee
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-09-10 18:51:33 -0400

    layout and buttons are set up, needs arranged and styled

--- src/data.js
+++ src/data.js
@@ -1,14 +1,10 @@
-// STEP 1 - Get the modules here exported before building out your components
-// Don't worry about bringing all of these into the same file.
-// Export them as necessary and import each array into its appropriate
-// file. No real tricks here just be aware of what is in each array
-// and how you'll access the data.
+
 
 //Initial Commit
 
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+export const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
 
-const operators = [
+export const operators = [
   {
     char: "/",
     value: "/"
@@ -31,4 +27,15 @@ const operators = [
   }
 ];
 
-const specials = ["C", "+/-", "%"];
+export const specials = ["C", "+/-", "%"];
+
+
+// STEP 1 - Get the modules here exported before building out your components
+// Don't worry about bringing all of these into the same file.
+// Export them as necessary and import each array into its appropriate
+// file. No real tricks here just be aware of what is in each array
+// and how you'll access the data.
+
+
+
+// export { numbers, operators, specials };
