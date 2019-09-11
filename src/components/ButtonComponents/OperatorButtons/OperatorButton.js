5799b3bf2562d0e9cf993635719df768cee48f40
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-09-11 11:04:37 -0400

    updated class names in buttons and tweaked style

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const OperatorButton = props => {
   return (
-    <button className="button operator">
+    <button className="buttonSuperOperator">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.operatorData}
     </button>
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SpecialButton = props => {
   return (
-    <button className="button special">
+    <button className="buttonSuperSpecial">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.specialData}
     </button>
