0ca2b06ea307ca83b7a208f66c55e846d5835d65
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-07 17:10:41 -0400

    Styled container, display, buttons, logo for MVP

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const OperatorButton = props => {
   return (
-   <button className = 'buttonOperators'>
+   <button className = 'operatorsButton'>
      {props.operatorData}
    </button>
   );
